12b22bbcd2664ed79857d9d1e7f1530a60931da7
tanakorn0314 <tanakorn0412@gmail.com>
2019-11-06 22:01:42 +0700

    Updates

---
+++ precache-manifest.1a6ca6512afbd4345e1e9a15203ec95b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bd654adb12986535c7831241ba4f34",
+    "url": "/take-a-break-app/index.html"
+  },
+  {
+    "revision": "d59dff433e490ea338c6",
+    "url": "/take-a-break-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "68636ec268f61d408b4e",
+    "url": "/take-a-break-app/static/js/2.443a4f86.chunk.js"
+  },
+  {
+    "revision": "d59dff433e490ea338c6",
+    "url": "/take-a-break-app/static/js/main.f98a7894.chunk.js"
+  },
+  {
+    "revision": "f2edb110459745821bc1",
+    "url": "/take-a-break-app/static/js/runtime-main.33556535.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/take-a-break-app/static/media/logo.25bf045c.svg"
+  }
+]);
